10aa73d06f357711480a7e8c9e54b52178ca9593
Briggs Elsperger <I3uckwheat@gmail.com>
2017-12-18 22:43:37 -0600

    adds start and stop functionality

--- stopWatch.js
+++ stopWatch.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+const timerDisplay = document.getElementById("stopWatchDisplay");
+const startStopButton = document.getElementById("startStop");
+let interval;
+let timerValue = 0;
+
+startStopButton.addEventListener("click", () => {
+  if(interval){
+    stopClock();
+  } else {
+    startClock();
+  }
+})
+
+function startClock(){
+  interval = setInterval(() => {
+    timerValue++
+    timerDisplay.textContent = timerValue / 100;
+  }, 10)
+}
+
+function stopClock(){
+  clearInterval(interval);
+  interval = null;
+}
+
+function reset(){
+  timerValue = 0
+  timerDisplay.textContent = 0;
+}
